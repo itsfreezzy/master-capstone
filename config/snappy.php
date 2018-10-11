de5bc406b137a509b278dfbe474352859bdad158
itsfreezzy <35260158+itsfreezzy@users.noreply.github.com>
2018-10-11 15:41:34 +0800

    Update config/snappy.php

--- config/snappy.php
+++ config/snappy.php
@@ -5,14 +5,14 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => base_path('vendor/wemersonjanuario/wkhtmltopdf/bin/64bit/wkhtmltopdf.exe'),
+        'binary'  => base_path('vendor/wemersonjanuario/wkhtmltopdf-windows/bin/64bit/wkhtmltopdf.exe'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => base_path('vendor/wemersonjanuario/wkhtmltopdf/bin/64bit/wkhtmltoimage.exe'),
+        'binary'  => base_path('vendor/wemersonjanuario/wkhtmltopdf-windows/bin/64bit/wkhtmltoimage.exe'),
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
